e1cace588d67e2ce7e056a4e84d48c6cef6aa198
RenanPlay13013 <renanluisdepaula13@outlook.com>
2026-05-26 19:41:49 -0300

    Corrected minimessage parser

--- src/main/java/net/loyalnetwork/loyalAutoSell/ui/formatter/MessageFormatter.java
+++ src/main/java/net/loyalnetwork/loyalAutoSell/ui/formatter/MessageFormatter.java
@@ -5,16 +5,28 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 public final class MessageFormatter {
+    private static final LegacyComponentSerializer LEGACY =
+            LegacyComponentSerializer.builder()
+                    .hexColors()
+                    .useUnusualXRepeatedCharacterHexFormat()
+                    .build();
+
+
+    private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
+    private static final MiniMessage MINI_MESSAGE_UNESCAPED = MiniMessage.builder()
+            .preProcessor(s -> s)
+            .build();
 
     public static Component parse(String message) {
         if (message == null || message.isBlank()) return Component.empty();
 
         String normalized = message.replace('§', '&');
+        Component legacyComponent = LEGACY.deserialize(normalized);
 
-        if (normalized.contains("<")) {
-            return MiniMessage.miniMessage().deserialize(normalized);
-        }
+        // Serializa sem escapar as tags já existentes
+        String serialized = MINI_MESSAGE.serialize(legacyComponent)
+                .replace("\\<", "<");
 
-        return LegacyComponentSerializer.legacyAmpersand().deserialize(normalized);
+        return MINI_MESSAGE.deserialize(serialized);
     }
 }
